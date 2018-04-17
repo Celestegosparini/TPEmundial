c4077f0f89695835c3597d6bc22b8ca9e79ed0d4
Celeste <celegos3@gmail.com>
2018-04-17 15:13:31 -0300

    masin on paridos

--- TestMundialFutbol/src/testmundialfutbol/TestMundialFutbol.java
+++ TestMundialFutbol/src/testmundialfutbol/TestMundialFutbol.java
@@ -2,34 +2,30 @@ package testmundialfutbol;
 
 import java.util.Date;
 
-
-
 public class TestMundialFutbol {
 
     public static void main(String[] args) {
 // seteo nombre de equipo
-        Equipo ALocal= new Equipo("Niupi");
-      
+        Equipo ALocal = new Equipo("Niupi");
+        System.out.println(ALocal.getNombre());
+
         Equipo BVisitante = new Equipo("Shutetsu");
-    
+        System.out.println(BVisitante.getNombre());
         Equipo CLocal = new Equipo("Furano");
-     
+        System.out.println(CLocal.getNombre());
+
         Equipo Dvisititante = new Equipo("Toho");
-       
-//   creo  PARTIDO 
-        Partido priPar = new Partido(new Date(2,4,18),ALocal,BVisitante);
-        
-        
-        Partido dosPar = new Partido(new Date(3,4,18), CLocal, Dvisititante);
-        
-        
-        
-        
-        
-     
-    
-    
-    
+        System.out.println(Dvisititante.getNombre());
+
+//   creo  PARTIDOs y adigno resultados 
+        Partido priPar = new Partido(new Date(2, 4, 18), ALocal, BVisitante);
+        Resultado priParResu = new Resultado();
+        priParResu.setResultado(3, 2);
+        priPar.setResultado(priParResu);    
+        Partido dosPar = new Partido(new Date(3, 4, 18), CLocal, Dvisititante);
+        Resultado dosParResu = new Resultado();
+        dosParResu.setResultado(4, 5);
+        dosPar.setResultado(dosParResu);   
     }
 
 }
